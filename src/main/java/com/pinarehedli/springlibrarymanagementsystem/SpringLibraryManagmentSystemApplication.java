83cd5a129f046e00350aca553c86c68484ff1753
Pınar <pinarehedli@gmail.com>
2025-07-11 12:23:59 +0400

    Initial commit

---
+++ src/main/java/com/pinarehedli/springlibrarymanagementsystem/SpringLibraryManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pinarehedli.springlibrarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinarehedli/springlibrarymanagementsystem/SpringLibraryManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinarehedli.springlibrarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
